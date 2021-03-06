deb46fa7ea5e609f47055d2cde241644190bd7f4
wsluser1 <wsluser1@DESKTOP-D3T5842>
2021-03-06 18:31:33 +0500

    ...

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ from datacenter.models import Passcard, Visit
 if __name__ == "__main__":
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())
+    print(Passcard.objects.all())
+    
