212ae3f436af39f7dfe20bc0a231ce6cf2126ccf
wsluser1 <wsluser1@DESKTOP-D3T5842>
2021-03-06 20:03:11 +0500

    step5

--- main.py
+++ main.py
@@ -12,5 +12,8 @@ if __name__ == "__main__":
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())
     print(Passcard.objects.all())
-    
-    
+    passcard = Passcard.objects.get(id=1)
+    print(passcard)
+    for field in (['owner_name', 'passcode', 'created_at', 'is_active']):
+      print('{}: {}'.format(field, passcard.__dict__.get(field)))
+    print(Passcard.objects.filter(is_active=True).count())    
