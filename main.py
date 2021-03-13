74231010d81d075ee59c4e0829fab8f40a6e87e5
vladimir-acer <vladimir@DESKTOP-D3T5842>
2021-03-13 09:49:19 +0500

    step8

--- main.py
+++ main.py
@@ -10,21 +10,25 @@ from datacenter.models import Passcard, Visit
 
 
 def format_duration(duration):
-    return "{}часа {}минут".format(duration.hours, duration.minutes)
+    hours = int(duration.total_seconds() // 3600)
+    minutes = int(duration.total_seconds() % 3600 // 60)
+    if hours == 0:
+        return "{} мин".format(minutes)
+    else:
+        return "{} ч {} мин".format(hours, minutes)
 
 
 if __name__ == "__main__":
     print('Количество пропусков:', Passcard.objects.count())
     print(Passcard.objects.all())
+    print("Активных пропусков: {}".format(Passcard.objects.filter(is_active=True).count()))
     passcard = Passcard.objects.get(id=1)
-    print(passcard)
+    print("Пропуск с id=1:", passcard)
     for field in (['owner_name', 'passcode', 'created_at', 'is_active']):
         print('{}: {}'.format(field, passcard.__dict__.get(field)))
-    print("Активных пропусков: {}".format(Passcard.objects.filter(is_active=True).count()))
-    # print(Visit.objects.all())
-    # strange_visits = Visit.objects.filter(leaved_at=None)
-    for visit in Visit.objects.filter(leaved_at=None):
-        print(visit.passcard.owner_name)
-    print(Visit.objects.filter(passcard__id=passcard.pk).count())
+    lst_in_storage = [visit.passcard.owner_name for visit in Visit.objects.filter(leaved_at=None)]
+    print("Список людей в хранилище:", lst_in_storage)
+    print("Число визитов в хранилище человека с пропуском id=1({1}): {0}".format(Visit.objects.filter(
+        passcard__id=passcard.pk).count(), passcard.owner_name))
     print('Визиты дольше 1000 мин:', len([item for item in Visit.objects.all() if item.is_visit_long(
             minutes=60)]))
